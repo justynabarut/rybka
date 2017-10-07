494db502b501d1f64565a46600b5fd2c28bd6ba1
Justyna Barut <justyna.barut@gmail.com>
2017-10-07 16:55:07 +0200

    initial commit with postgres

---
+++ test/controllers/welcomes_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class WelcomesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get index" do
+    get welcomes_url
+    assert_response :success
+  end
+
+end
